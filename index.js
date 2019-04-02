e5636824fd61d8f42db3189e1f06e1036b8e47df
rajeevgopalakrishna <rajeevgopalakrishna@gmail.com>
2019-04-02 18:30:58 +0530

    Extended to other data

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const fs = require("fs-extra");
 
-function buildAstData(sources) {
-  const header = "JSON AST (compact format):\n";
-  const data = Object.keys(sources).map((key) => {
-    return `
-======= ${key} =======
-
-${JSON.stringify(sources[key].ast, null, 2)}
-`;
-  });
-  return header + data.join("\n");
+function buildContractData(embark) {
+    const sources = embark.sources;
+    const contracts = embark.contracts;
+    const data1 = Object.keys(sources).map((key) => {
+	return `${JSON.stringify(sources[key].ast, null, 2)}`;
+    });
+    const data2 = Object.keys(contracts).map((key) => {
+	return `${JSON.stringify(contracts[key], null, 2)}`;
+    });
+    return '{\n' + '\"embark-asts\":' + data1.join("\n") + ',\n' + '\"embark-other\":' + data2.join("\n") + '\n}';
 }
 
 async function run(embark, compilationResult) {
-    const astFile = "embark.ast.json";
-    const astData = buildAstData(compilationResult.sources);
-    fs.openSync(astFile, 'w');
-    fs.writeFileSync(astFile, astData);
-    fs.closeSync(astFile);
+    const contractDataFile = "embark.contractData.json";
+    const contractData = buildContractData(compilationResult);
+    fs.openSync(contractDataFile, 'w');
+    fs.writeFileSync(contractDataFile, contractData);
+    fs.closeSync(contractDataFile);
 }
 
 function register(embark) {
