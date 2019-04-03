a837d12deb4b17e958a2ff4cc39ffa2699d1a1ee
Josselin <josselin@trailofbits.com>
2019-04-03 16:02:20 +0100

    Dont save contracts from the node_modules repo

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const fs = require("fs-extra");
 
-function buildContractInfo(embark) {
-    const sources = embark.sources;
-    const contracts = embark.contracts;
-    const data1 = Object.keys(sources).map((key) => {
-	return `${JSON.stringify(sources[key].ast, null, 2)}`;
+
+function buildContractInfo(compilationResult) {
+    const sources = compilationResult.sources;
+    const keys_sources =  Object.keys(sources)
+    // embark might have dupplicate source and contracts
+    // so we removed all the elements that are from the modules
+    const keys_sources_filtered = keys_sources.filter(function(key){ 
+        return ! key.startsWith("node_modules"); 
+    }); 
+    const asts = keys_sources_filtered.map((key) => {
+	    return `${JSON.stringify(sources[key].ast, null, 2)}`;
     });
-    const data2 = Object.keys(contracts).map((key) => {
-	return `${JSON.stringify(contracts[key], null, 2)}`;
+
+    const contracts = compilationResult.contracts;
+    const keys_contracts =  Object.keys(contracts)
+    const keys_contracts_filtered = keys_contracts.filter(function(key){ 
+        return ! key.startsWith("node_modules"); 
+    }); 
+    const contracts_info = keys_contracts_filtered.map((key) => {
+	    return `${JSON.stringify(contracts[key], null, 2)}`;
     });
-    return '{\n' + '\"asts\":[' + data1.join("\n,") + '],\n' + '\"contracts-info\":[' + data2.join("\n,") + ']\n}';
+    return '{\n' + '\"asts\":[' + asts.join("\n,") + '],\n' + '\"contracts-info\":[' + contracts_info.join("\n,") + ']\n}';
 }
 
 async function run(embark, compilationResult) {
